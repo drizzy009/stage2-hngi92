b080171d6c74d56872d63597d2548461ce0dd91d
Idris Yusuf <yusid03@gmail.com>
2022-11-05 10:49:50 -0700

    0

--- src/components/Notfound.js
+++ src/components/Notfound.js
@@ -1,9 +1,10 @@
-
+import { Link } from "react-router-dom"
 
 const Notfound = () => {
   return (
-    <div className="flex flex-col justify-center p-52">
-      <h3 className="text-8xl font-extrabold text-gray-800 flex flex-col items-center">404 Error Page</h3>
+    <div className="flex flex-col justify-center py-52">
+      <h3 className="text-8xl font-extrabold text-gray-800 flex flex-col justify-center items-center">404 Error Page</h3>
+      <Link className="flex flex-col justify-center items-center py-4 font-semibold text-3xl text-red-700 underline" to="/" >Go back</Link>
     </div>
   )
 }

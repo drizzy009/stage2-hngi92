faeb609185906fde6f6e6e5c9e46eb4971786f53
Idris Yusuf <yusid03@gmail.com>
2022-11-05 10:01:46 -0700

    contact

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,7 +1,61 @@
 import styles from "../style.module.css";
 import Footer from "./Footer";
+import { useEffect, useState } from "react";
 
 const Contact = () => {
+  const [formvalue, setFormvalue] = useState({
+    fname: "",
+    lname: "",
+    email: "",
+    message: "",
+  });
+  const [formerror, setFormerror] = useState({});
+  const [issubmit, setSubmit] = useState(false);
+
+  const handlevalidation = (e) => {
+    const { name, value } = e.target;
+    setFormvalue({ ...formvalue, [name]: value });
+  };
+  const handlesubmit = (e) => {
+    e.preventDefault();
+    setFormerror(validationform(formvalue));
+    setSubmit(true);
+  };
+  const validationform = (value) => {
+    const errors = {};
+    const emailPattern =
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()\\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+    if (!value.fname) {
+      errors.fname = "Please First Enter Name";
+    }
+    if (!value.lname) {
+      errors.lname = "Please Your Enter Name";
+    }
+
+    if (!value.email) {
+      errors.email = "Please Enter Email";
+    } else if (!emailPattern.test(value.email)) {
+      errors.email = "Enter Valid Email";
+    }
+    if (!value.message) {
+      errors.message = "Please Enter a message";
+    }
+
+    return errors;
+  };
+
+  useEffect(() => {
+    if (Object.keys(formerror).length === 0 && issubmit) {
+      console.log(formvalue);
+    }
+  }, [formerror, formvalue, issubmit]);
+
+
+  console.log(formvalue.fname)
+  console.log(formvalue.lname)
+  console.log(formvalue.email)
+
   return (
     <div>
       <div className={`${styles.contactPage}`}>
@@ -11,11 +65,11 @@ const Contact = () => {
               <div className={`${styles.contactHeader}`}>
                 <h3 className={`${styles.contactHeader_h3}`}>Contact Me</h3>
                 <p className={`${styles.contactHeader_p}`}>
-                  Hi there, contact me to ask me about <br className="sm:hidden" /> anything you have in
-                  mind.
+                  Hi there, contact me to ask me about{" "}
+                  <br className="sm:hidden" /> anything you have in mind.
                 </p>
               </div>
-              <div className={`${styles.contactForm}`}>
+              <form onSubmit={ handlesubmit} className={`${styles.contactForm}`}>
                 <div className={`${styles.contactFormfields}`}>
                   <div className={`${styles.nameField}`}>
                     <label
@@ -28,8 +82,11 @@ const Contact = () => {
                         type="text"
                         name="fname"
                         placeholder="Enter your first name"
+                        value={ formvalue.fname}  onChange={ handlevalidation}
                         required
                       />
+                      <br/>
+                    <span className={`${styles.formHint}`}>{ formerror.fname }</span>
                     </label>
                     <label
                       htmlFor="Last name"
@@ -41,8 +98,11 @@ const Contact = () => {
                         type="text"
                         name="lname"
                         placeholder="Enter your last name"
+                        value={ formvalue.lname}  onChange={ handlevalidation}
                         required
                       />
+                      <br/>
+                    <span className="text-danger">{ formerror.lname } </span>
                     </label>
                   </div>
                   <div className={`${styles.emailField}`}>
@@ -53,8 +113,11 @@ const Contact = () => {
                         type="email"
                         name="email"
                         placeholder="yourname@email.com"
+                        value={ formvalue.email}  onChange={ handlevalidation}
                         required
                       />
+                      <br/>
+                    <span className="text-danger">{ formerror.email } </span>
                     </label>
                   </div>
                   <div className={`${styles.textField}`}>
@@ -67,7 +130,10 @@ const Contact = () => {
                         id=""
                         rows="4"
                         cols="4"
+                        value={ formvalue.message}  onChange={ handlevalidation}
                       ></textarea>
+                      <br/>
+                    <span className="text-danger">{ formerror.message } </span>
                     </label>
                   </div>
                   <div className={`${styles.formCheckbox}`}>
@@ -76,20 +142,24 @@ const Contact = () => {
                       type="checkbox"
                       name="checkbox"
                       id=""
+                      required
                     />
                     <label
                       htmlFor="checkbox"
                       className={`${styles.formLabels}`}
                     >
-                      You agree to providing your data to <br className="sm:hidden" /> {`Drizzy0x`} who may
-                      contact you.
+                      You agree to providing your data to{" "}
+                      <br className="sm:hidden" /> {`Drizzy0x`} who may contact
+                      you.
                     </label>
                   </div>
                   <div className={`${styles.formButton}`}>
-                    <input className={`${styles.formInputsBtn}`} type="submit" value="Send message" required />
+                    <button className={`${styles.formInputsBtn}`}>
+                      Send message
+                    </button>
                   </div>
-                </div>
               </div>
+              </form>
             </div>
           </div>
         </div>
